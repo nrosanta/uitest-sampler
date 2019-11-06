58ccc5189a65329ec3b8f1e5c97f0f9907542572
Ross Meitei <nrosanta@gmail.com>
2019-11-05 22:51:34 -0500

    Updated tests

--- src/test/java/com/ui/tests/DataProviderImpl.java
+++ src/test/java/com/ui/tests/DataProviderImpl.java
@@ -1,23 +1,22 @@
 package com.ui.tests;
 
 import java.io.FileInputStream;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
 
 public class DataProviderImpl {
-	@DataProvider(name="data-provider")
+	@DataProvider(name="common-test-data")
 	public Object[][] dataProviderMethod(){
 		String filename="src/test/resources/test-data/TestData.xlsx";
 		String sheetName="Links";
-		XSSFSheet excelWSheet;
+		XSSFSheet excelWSheet = null;
 		XSSFWorkbook excelWBook;
-		XSSFCell cell;
-		XSSFRow row;
-		
-		
+	
 		try {
 			FileInputStream excel = new FileInputStream(filename);
 			excelWBook = new XSSFWorkbook(excel);
@@ -27,8 +26,45 @@ public class DataProviderImpl {
 			e.printStackTrace();
 		}
 		
+		Object[][] obj = new Object[1][1];
+		obj[0][0] = getData(excelWSheet);
 		
-		return null;
+		return obj;
+		
+	}
+	
+	public Map<String, Map<String, String>> getData(XSSFSheet worksheet) {
+		int colHeaderCount = worksheet.getRow(0).getLastCellNum();
+		int rowCount = worksheet.getLastRowNum();
+		Map<String, Map<String, String>> dataMap = new HashMap<>();
+		List<String> headers = new ArrayList<>();
+		
+		/*
+		 * Get top rows
+		 */
+		for(int i=0;i<colHeaderCount;i++) {
+			headers.add(worksheet.getRow(0).getCell(i).getStringCellValue());
+		}
+		
+		System.out.println("Headers: "+headers);
+		
+		/*
+		 * Get row data
+		 */
+		//Iterator<Row> rowItr = worksheet.rowIterator();
+		for(int i=1;i<rowCount;i++) { //skip 1st row
+			//XSSFRow row = (XSSFRow) rowItr.next();
+			Map<String, String> rowMap = new HashMap<>();
+			for(int j=0;j<headers.size();j++) {
+				String cellVal = worksheet.getRow(i).getCell(j).getStringCellValue();
+				//System.out.println("Data: "+cellVal);
+				rowMap.put(headers.get(j), cellVal);
+				
+			}
+			dataMap.put(worksheet.getRow(i).getCell(0).getStringCellValue(), rowMap);
+			
+		}
+		return dataMap;
 		
 	}
 
--- src/test/java/com/ui/tests/TestSuite.java
+++ src/test/java/com/ui/tests/TestSuite.java
@@ -1,4 +1,6 @@
 package com.ui.tests;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterSuite;
@@ -23,15 +25,15 @@ public class TestSuite {
 		driver.quit();
 	}
 	
-	@Test (description = "Validate Home Page Title", priority=1)
-	public void validatePageTitle() {
+	@Test (dataProvider="common-test-data", dataProviderClass=com.ui.tests.DataProviderImpl.class, description = "Validate Home Page Title", priority=1)
+	public void validatePageTitle(Map<String, Map<String, String>> dataMap) {
 		String url = "http://www.namaonline.org";
 		driver.get(url);
 		assertion.assertEquals(driver.getTitle(), "testingfly.com","Unexpected page title!");
 	}
 	
-	@Test(description = "Validate Home Page Link", priority=2)
-	public void clickHomePage() {
+	@Test(dataProvider="common-test-data", dataProviderClass=com.ui.tests.DataProviderImpl.class, description = "Validate Home Page Link", priority=2)
+	public void clickHomePage(Map<String, Map<String, String>> dataMap) {
 		driver.findElement(By.xpath("//*[@id=\"cssmenu\"]/ul/li[1]/a/span")).click();
 		assertion.assertEquals(driver.getCurrentUrl(), "http://www.namaonline.org1");
 		
